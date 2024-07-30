c3c9bb61d2c8c929ed08747308190486bcf67928
Raviteja Gangirddy <raviteja.g0717@gmail.com>
2024-07-30 10:41:08 +0530

    Added docker file

--- src/main/java/com/upskill/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/upskill/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
@@ -1,12 +1,21 @@
 package com.upskill.dockerjenkinsintegration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinsIntegrationApplication {
 
+    public static Logger logger = LoggerFactory.getLogger(DockerJenkinsIntegrationApplication.class);
+
+    public void init() {
+        logger.info("Welcome to DockerJenkinsIntegration.....");
+    }
+
     public static void main(String[] args) {
+        logger.info("Welcome to DockerJenkinsIntegration completed....");
         SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
     }
 
